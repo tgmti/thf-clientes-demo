cfc554bbcbdd418aa7ad532520a8f40ffaf0a121
TGM - Thiago Mota <tgmspawn@gmail.com>
2019-05-15 22:07:43 -0300

    Adicionada tabela para listagem de clientes
     - Adicionado componente thf-table no HTML
    
    - Adicionado acesso ao serviço de teste e a subscrição para preencher o array de clientes

--- src/app/clientes/cliente-list/cliente-list.component.ts
+++ src/app/clientes/cliente-list/cliente-list.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
+import { Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-cliente-list',
   templateUrl: './cliente-list.component.html',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClienteListComponent implements OnInit {
 
-  constructor() { }
+  // Url do servidor de exemplo
+  private readonly url: string = 'https://sample-customers-api.herokuapp.com/api/thf-samples/v1/people';
+  private clienteSub = Subscription;
+
+  public clientes: Array<any> = [];
+
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
+    this.clienteSub = this.httpClient.get(this.url)
+      .subscribe((response: {hasNext: boolean, items: Array<any>}) => {
+        this.clientes = response.items
+      });
+  }
+
+  ngOnDestroy() {
+    this.clienteSub.unsubscribe();
   }
 
 }
