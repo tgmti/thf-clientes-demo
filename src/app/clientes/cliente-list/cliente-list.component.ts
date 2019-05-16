fd6477e2986c9d9654ac1d2d85a8a5ee3dcbff22
TGM - Thiago Mota <tgmspawn@gmail.com>
2019-05-15 22:24:34 -0300

    Adicionada coluna e-mail
     - Adicionada coluna com action apontando para o método sendMail
    
     - Adicionado método sendMail
    
     - Adicionados Labels para as colunas
    
     - Adicionada coluna birthdate (Data de Nascimento)

--- src/app/clientes/cliente-list/cliente-list.component.ts
+++ src/app/clientes/cliente-list/cliente-list.component.ts
@@ -20,8 +20,10 @@ export class ClienteListComponent implements OnInit {
 
   public readonly colunas: Array<ThfTableColumn> = [
     // Definição das colunas
-    {property: 'name'},
-    {property: 'nickname'}
+    {property: 'name', label: 'Nome Completo'},
+    {property: 'nickname', label: 'Usuário' },
+    {property: 'email', label: 'E-mail', type: 'link', action: this.sendMail.bind(this)},
+    {property: 'birthdate', label: 'Nascimento', type: 'date', format: 'dd/MM/yyyy' },
   ];
 
   constructor(private httpClient: HttpClient) { }
@@ -35,6 +37,13 @@ export class ClienteListComponent implements OnInit {
 
   ngOnDestroy() {
     this.clienteSub.unsubscribe();
+  }
+
+  private sendMail(email, cliente) {
+    const body = `Olá ${cliente.name}, gostaríamos de agradecer seu contato.`;
+    const subject = `Contato - Cliente ${cliente.name}`;
+
+    window.open(`mailto:${email}?subject=${subject}&body=${body}`, '_self');
   }
 
 }
