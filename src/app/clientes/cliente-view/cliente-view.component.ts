01e2185e3f2fde0c0861d3fec16bfc928f61f4eb
TGM - Thiago Mota <tgmspawn@gmail.com>
2019-05-18 06:52:23 -0300

    Visualização de cliente adicionada requisição para o backend

--- src/app/clientes/cliente-view/cliente-view.component.ts
+++ src/app/clientes/cliente-view/cliente-view.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-cliente-view',
   templateUrl: './cliente-view.component.html',
   styleUrls: ['./cliente-view.component.css']
 })
-export class ClienteViewComponent implements OnInit {
+export class ClienteViewComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private readonly url: string = 'https://sample-customers-api.herokuapp.com/api/thf-samples/v1/people';
+
+  private customerSub: Subscription;
+  private paramsSub: Subscription;
+
+  constructor(private httpClient: HttpClient, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.paramsSub = this.route.params.subscribe(params => this.loadData(params['id']));
+  }
+
+  ngOnDestroy() {
+    this.paramsSub.unsubscribe();
+    this.customerSub.unsubscribe();
+  }
+
+  /** Função que carrega os dados do cliente */
+  private loadData(id) {
+    this.customerSub = this.httpClient.get(`${this.url}/${id}`).subscribe(console.log);
   }
 
 }
